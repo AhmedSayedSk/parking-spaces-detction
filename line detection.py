a418dbef05ba9938f54bf0e0aff4d6e89d748c1b
abdallahmohsen <abdullahmohsensayed@gmail.com>
2019-04-30 13:55:59 +0200

    First Commit

---
+++ line detection.py
@@ -0,0 +1,47 @@
+import sys
+import math
+import cv2 as cv
+import numpy as np
+
+
+def main(argv):
+    #efault_file = 'Empty1.jpg'
+    default_file = 'Empty2.jpg'
+    #efault_file = 'Empty3.png'
+    #efault_file = 'empty_lot.jpg'
+    #default_file = 'parking_lot.png'
+
+    filename = argv[0] if len(argv) > 0 else default_file
+    # Loads an image
+    src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_GRAYSCALE)
+    # Check if image is loaded fine
+    if src is None:
+        print('Error opening image!')
+        print('Usage: hough_lines.py [image_name -- default ' + default_file + '] \n')
+        return -1
+
+    dst = cv.Canny(src, 250, 350, None, 3)
+    # here i can change the value of the min and max gradients depending on the brightness of the image
+    cv.imshow ("canny", dst)
+    # Copy edges to the images that will display the results in BGR
+    cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+
+    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
+
+
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 6, cv.LINE_AA)
+            #print(l[0], "  ", l[1], "  ", l[2], "  ", l[3])
+# cv.LINE_AA >> gives anti-aliased line which looks great for curves.
+    cv.imshow("Source", src)
+    cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+
+    cv.waitKey()
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ trash.py
@@ -0,0 +1,9 @@
+import numpy as np
+import numpy
+
+x = np.array( [[1,2,3],[5,9,3]] )
+print (x)
+y = x.shape
+
+print (y[0])
+print (y)
